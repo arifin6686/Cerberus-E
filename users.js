7b2006d4ec46527d58ec998527697b5a205a41ec
AHelpfulRayquaza <faheemawan214@gmail.com>
2018-07-29 21:23:26 +0530

    Update users.js

--- users.js
+++ users.js
@@ -2,7 +2,7 @@
 
 const cache_db = require("./cache-db.js");
 
-const developers = ["aflyingphantom", "ahelpfulrayquaza"];
+const developers = ["aflyingphantom", "fheem", "ahelpfulrayquaza"];
 let Users = {};
 let users = Users.users = new Map();
 Users.seen = new cache_db();
